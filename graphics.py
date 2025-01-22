5d4ebaebf056602adf556cc67b938941f238bbed
Mango <plais999@gmail.com>
2025-01-22 02:03:14 +0000

    Added Default Values
    Added some default values to the functions

--- graphics.py
+++ graphics.py
@@ -8,20 +8,20 @@ screen_height = 500
 screen_center = (round(screen_width*0.5),round(screen_height*0.5))
 game_run = True
 
-def innit_screen(w,h):
+def innit_screen(width=500,height=500):
     global screen
-    screen = pygame.display.set_mode((w, h))
+    screen = pygame.display.set_mode((width, height))
     screen.fill((200,200,200))
 
-def draw_rounded_rect(center_x,center_y,width,height,padding,colour_outer,colour_inner):
+def draw_rounded_rect(center_x,center_y,width,height,colour_outer=(45, 69, 68),colour_inner=(92, 125, 124),padding=10):
     x = center_x - round(width*0.5)
     y = center_y - round(height*0.5)
     pygame.draw.rect(screen, colour_outer, (x,y, width, height), 0, round(padding*1.5), round(padding*1.5), round(padding*1.5), round(padding*1.5))
     pygame.draw.rect(screen, colour_inner, (x+round(padding*0.5),y+round(padding*0.5), width-padding, height-padding), 0, padding, padding, padding, padding)
 
 innit_screen(screen_width, screen_height)
-draw_rounded_rect(screen_height/2, screen_width/2, 100, 150, 20, (50,50,50), (150,150,150))
-pygame.draw.circle(screen, (255,0,0),screen_center,5)
+
+draw_rounded_rect(screen_center[0],screen_center[1], 63, 88)
 
 while game_run:
     for event in pygame.event.get():
